78edfb3e8012294edf648a8bf97069f5da427728
smayan-gowda <smayan-gowda@users.noreply.github.com>
2025-12-31 17:44:21 -0600

    update interpretability init

---
+++ src/interpretability/__init__.py
@@ -0,0 +1,28 @@
+"""mechanistic interpretability tools for wav2vec2."""
+
+from .extraction import (
+    Wav2Vec2ActivationExtractor,
+    AttentionExtractor,
+    extract_activations_from_dataset,
+    load_activations_memmap
+)
+
+from .patching import (
+    ActivationPatcher,
+    AttentionHeadPatcher,
+    create_minimal_pairs,
+    compute_patching_importance,
+    compute_causal_contribution
+)
+
+__all__ = [
+    'Wav2Vec2ActivationExtractor',
+    'AttentionExtractor',
+    'extract_activations_from_dataset',
+    'load_activations_memmap',
+    'ActivationPatcher',
+    'AttentionHeadPatcher',
+    'create_minimal_pairs',
+    'compute_patching_importance',
+    'compute_causal_contribution'
+]
